c7f8ed6f2fa12dfee926f87edfd8797e9b8eacd5
lukasbia <stodomingokylijahnver@gmail.com>
2026-09-10 21:48:05 +0800

    Update generator.asm

--- Compiler/asc-c/src/generator.asm
+++ Compiler/asc-c/src/generator.asm
@@ -2,19 +2,38 @@ bits 64
 
 global asc_generate
 
-%define AST_PROGRAM       1
-%define AST_VARIABLE      2
-%define AST_CONSTANT      3
-%define AST_ASSIGNMENT    4
-%define AST_INTEGER       5
-%define AST_IDENTIFIER     6
-%define AST_BINARY        7
-%define AST_WHILE         8
+extern malloc
 
-%define TOKEN_PLUS        8
-%define TOKEN_MINUS       9
-%define TOKEN_MULTIPLY    10
-%define TOKEN_DIVIDE      11
+%define NODE_PROGRAM       1
+%define NODE_BLOCK         2
+%define NODE_VAR           3
+%define NODE_FUNCTION      4
+%define NODE_PARAMETER     5
+%define NODE_RETURN        6
+%define NODE_IF            7
+%define NODE_WHILE         8
+%define NODE_BINARY        9
+%define NODE_UNARY         10
+%define NODE_CALL          12
+%define NODE_IDENTIFIER    13
+%define NODE_INTEGER       14
+
+%define IR_SIZE            32
+%define IR_PROGRAM         1
+%define IR_FUNCTION        2
+%define IR_LABEL           3
+%define IR_LOAD_IMM        4
+%define IR_LOAD            5
+%define IR_STORE           6
+%define IR_ADD             7
+%define IR_SUB             8
+%define IR_MUL             9
+%define IR_DIV             10
+%define IR_COMPARE         11
+%define IR_JUMP            12
+%define IR_JUMP_ZERO       13
+%define IR_CALL            14
+%define IR_RETURN          15
 
 section .text
 
@@ -23,433 +42,266 @@ asc_generate:
     push rbp
     mov rbp, rsp
 
+    push rbx
     push r12
     push r13
-    push r14
-    push r15
 
     mov r12, rdi
-    mov r13, rsi
 
-    lea r14, [output_buffer]
-    xor r15, r15
-    xor rbx, rbx
+    mov edi, 32
+    call malloc
 
-    call generate_program
+    test rax, rax
+    jz .fail
 
-    mov byte [r14], 0
+    mov r13, rax
 
-    lea rax, [output_buffer]
-    mov rdx, r14
-    sub rdx, rax
+    mov dword [rax], IR_PROGRAM
+    mov qword [rax + 8], 0
+    mov qword [rax + 16], 0
+    mov qword [rax + 24], 0
 
-    pop r15
-    pop r14
+    mov r8, [r12 + 16]
+
+.loop:
+
+    test r8, r8
+    jz .done
+
+    mov rdi, r8
+    mov rsi, r13
+
+    call generate_node
+
+    test eax, eax
+    jz .fail
+
+    mov r8, [r8 + 24]
+    jmp .loop
+
+.done:
+
+    mov rax, r13
+
     pop r13
     pop r12
-
+    pop rbx
     pop rbp
+    ret
 
+.fail:
+
+    xor eax, eax
+
+    pop r13
+    pop r12
+    pop rbx
+    pop rbp
     ret
 
 
-generate_program:
+generate_node:
 
-    xor rcx, rcx
+    mov eax, [rdi]
 
-    lea rsi, [section_text]
-    call emit_string
+    cmp eax, NODE_FUNCTION
+    je generate_function
 
-program_loop:
-
-    cmp rcx, r13
-    jae program_finished
-
-    mov eax, [r12 + rcx * 32]
-
-    cmp eax, AST_VARIABLE
+    cmp eax, NODE_VAR
     je generate_variable
 
-    cmp eax, AST_CONSTANT
-    je generate_constant
+    cmp eax, NODE_RETURN
+    je generate_return
 
-    cmp eax, AST_ASSIGNMENT
-    je generate_assignment
+    cmp eax, NODE_BLOCK
+    je generate_block
 
-    cmp eax, AST_WHILE
+    cmp eax, NODE_IF
+    je generate_if
+
+    cmp eax, NODE_WHILE
     je generate_while
 
-    inc rcx
-    jmp program_loop
+    mov eax, 1
+    ret
 
-program_finished:
 
+generate_function:
+
+    mov eax, 1
     ret
 
 
 generate_variable:
 
-    mov r8, [r12 + rcx * 32 + 8]
-    mov r9, [r12 + rcx * 32 + 24]
+    mov r8, [rdi + 24]
 
-    lea rsi, [variable_prefix]
-    call emit_string
+    test r8, r8
+    jz .done
 
-    mov rsi, r8
-    mov rdx, 0
+    mov rdi, r8
+    call generate_expression
 
-    call emit_identifier
+.done:
+    mov eax, 1
+    ret
 
-    lea rsi, [variable_suffix]
-    call emit_string
+
+generate_return:
+
+    mov r8, [rdi + 8]
+
+    test r8, r8
+    jz .empty
+
+    mov rdi, r8
+    call generate_expression
+
+.empty:
+    mov eax, 1
+    ret
+
+
+generate_block:
+
+    mov r8, [rdi + 16]
+
+.loop:
+
+    test r8, r8
+    jz .done
+
+    mov rdi, r8
+    call generate_node
+
+    test eax, eax
+    jz .fail
+
+    mov r8, [r8 + 24]
+    jmp .loop
+
+.done:
+    mov eax, 1
+    ret
+
+.fail:
+    xor eax, eax
+    ret
+
+
+generate_if:
+
+    mov r8, [rdi + 8]
+    mov r9, [rdi + 16]
+
+    test r8, r8
+    jz .fail
 
     test r9, r9
-    jz variable_next
+    jz .fail
 
-    mov rdi, r9
+    mov rdi, r8
     call generate_expression
 
-variable_next:
+    mov rdi, r9
+    call generate_node
 
-    lea rsi, [newline]
-    call emit_string
+    mov r8, [rdi + 24]
 
-    inc rcx
-    jmp program_loop
+    test r8, r8
+    jz .done
+
+    mov rdi, r8
+    call generate_node
+
+.done:
+    mov eax, 1
+    ret
+
+.fail:
+    xor eax, eax
+    ret
 
 
-generate_constant:
+generate_while:
 
-    mov r8, [r12 + rcx * 32 + 8]
-    mov r9, [r12 + rcx * 32 + 24]
+    mov r8, [rdi + 8]
+    mov r9, [rdi + 16]
 
-    lea rsi, [constant_prefix]
-    call emit_string
-
-    mov rsi, r8
-    call emit_identifier
-
-    lea rsi, [constant_suffix]
-    call emit_string
+    test r8, r8
+    jz .fail
 
     test r9, r9
-    jz constant_next
+    jz .fail
 
-    mov rdi, r9
+    mov rdi, r8
     call generate_expression
 
-constant_next:
-
-    lea rsi, [newline]
-    call emit_string
-
-    inc rcx
-    jmp program_loop
-
-
-generate_assignment:
-
-    mov r8, [r12 + rcx * 32 + 8]
-    mov r9, [r12 + rcx * 32 + 16]
-
-    lea rsi, [assignment_prefix]
-    call emit_string
-
-    mov rsi, r8
-    call emit_identifier
-
-    lea rsi, [assignment_middle]
-    call emit_string
-
     mov rdi, r9
-    call generate_expression
+    call generate_node
 
-    lea rsi, [newline]
-    call emit_string
+    mov eax, 1
+    ret
 
-    inc rcx
-    jmp program_loop
+.fail:
+    xor eax, eax
+    ret
 
 
 generate_expression:
 
     mov eax, [rdi]
 
-    cmp eax, AST_INTEGER
-    je generate_integer
+    cmp eax, NODE_INTEGER
+    je .integer
 
-    cmp eax, AST_IDENTIFIER
-    je generate_identifier
+    cmp eax, NODE_IDENTIFIER
+    je .identifier
 
-    cmp eax, AST_BINARY
-    je generate_binary
+    cmp eax, NODE_BINARY
+    je .binary
 
+    cmp eax, NODE_CALL
+    je .call
+
+    mov eax, 1
     ret
 
+.integer:
 
-generate_integer:
-
-    mov rax, [rdi + 8]
-
-    lea rsi, [integer_prefix]
-    call emit_string
-
-    mov rsi, rax
-    call emit_integer
-
+    mov eax, IR_LOAD_IMM
     ret
 
+.identifier:
 
-generate_identifier:
-
-    mov rsi, [rdi + 8]
-
-    lea rdx, [rdi + 16]
-
-    call emit_identifier
-
+    mov eax, IR_LOAD
     ret
 
+.binary:
 
-generate_binary:
+    mov r8, [rdi + 8]
+    mov r9, [rdi + 16]
 
-    push rdi
+    test r8, r8
+    jz .fail
 
-    mov r8, [rdi + 16]
-    mov r9, [rdi + 24]
-    mov r10, [rdi + 8]
+    test r9, r9
+    jz .fail
 
     mov rdi, r8
     call generate_expression
-
-    lea rsi, [space]
-    call emit_string
-
-    cmp r10, TOKEN_PLUS
-    je binary_plus
-
-    cmp r10, TOKEN_MINUS
-    je binary_minus
-
-    cmp r10, TOKEN_MULTIPLY
-    je binary_multiply
-
-    cmp r10, TOKEN_DIVIDE
-    je binary_divide
-
-    pop rdi
-    ret
-
-
-binary_plus:
-
-    lea rsi, [plus]
-    call emit_string
-    jmp binary_right
-
-
-binary_minus:
-
-    lea rsi, [minus]
-    call emit_string
-    jmp binary_right
-
-
-binary_multiply:
-
-    lea rsi, [multiply]
-    call emit_string
-    jmp binary_right
-
-
-binary_divide:
-
-    lea rsi, [divide]
-    call emit_string
-
-
-binary_right:
-
-    lea rsi, [space]
-    call emit_string
 
     mov rdi, r9
     call generate_expression
 
-    pop rdi
+    mov eax, IR_ADD
     ret
 
+.call:
 
-generate_while:
-
-    mov r8, [r12 + rcx * 32 + 8]
-
-    lea rsi, [while_prefix]
-    call emit_string
-
-    mov rdi, r8
-    call generate_expression
-
-    lea rsi, [while_suffix]
-    call emit_string
-
-    inc rcx
-    jmp program_loop
-
-
-emit_string:
-
-    mov al, [rsi]
-
-    test al, al
-    jz emit_string_finished
-
-    mov [r14], al
-    inc r14
-    inc rsi
-
-    jmp emit_string
-
-emit_string_finished:
-
+    mov eax, IR_CALL
     ret
 
-
-emit_identifier:
-
-    mov al, [rsi]
-
-    test al, al
-    jz emit_identifier_finished
-
-    cmp al, ' '
-    je emit_identifier_finished
-
-    cmp al, 10
-    je emit_identifier_finished
-
-    mov [r14], al
-    inc r14
-    inc rsi
-
-    jmp emit_identifier
-
-emit_identifier_finished:
-
+.fail:
+    xor eax, eax
     ret
-
-
-emit_integer:
-
-    push rax
-    push rbx
-    push rcx
-    push rdx
-
-    test rsi, rsi
-    jnz integer_positive
-
-    mov byte [r14], '0'
-    inc r14
-    jmp integer_finished
-
-
-integer_positive:
-
-    xor rcx, rcx
-    mov rax, rsi
-    mov rbx, 10
-
-integer_convert:
-
-    xor rdx, rdx
-    div rbx
-
-    add dl, '0'
-
-    push rdx
-
-    inc rcx
-
-    test rax, rax
-    jnz integer_convert
-
-
-integer_write:
-
-    pop rax
-
-    mov [r14], al
-    inc r14
-
-    loop integer_write
-
-
-integer_finished:
-
-    pop rdx
-    pop rcx
-    pop rbx
-    pop rax
-
-    ret
-
-
-section .data
-
-section_text:
-    db "section .text", 10
-    db "global _start", 10
-    db "_start:", 10, 0
-
-variable_prefix:
-    db "    mov qword [", 0
-
-variable_suffix:
-    db "], 0", 0
-
-constant_prefix:
-    db "    mov qword [", 0
-
-constant_suffix:
-    db "], 0", 0
-
-assignment_prefix:
-    db "    mov rax, [", 0
-
-assignment_middle:
-    db "]", 10
-    db "    ", 0
-
-while_prefix:
-    db "while_", 0
-
-while_suffix:
-    db ":", 10, 0
-
-integer_prefix:
-    db "    mov rax, ", 0
-
-space:
-    db " ", 0
-
-plus:
-    db "+", 0
-
-minus:
-    db "-", 0
-
-multiply:
-    db "*", 0
-
-divide:
-    db "/", 0
-
-newline:
-    db 10, 0
-
-
-section .bss
-
-output_buffer:
-    resb 1048576
